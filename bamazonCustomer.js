0bee9d45bb121fe33780d0492d7dddb97770bec1
Renee Hickman <renee.hickman@outlook.com>
2019-03-04 15:50:19 -0600

    database setup and query all products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,7 +18,36 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    // runSearch();
+    queryAllProducts();
 });
 
 
-    
+function queryAllProducts(){
+    console.log("\nDisplaying all products available for sale...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+        console.log("-------------------------------------------");
+        for (var i = 0; i < res.length; i++) {
+          console.log(res[i].product_id + " | " + res[i].product_name + " | " + "$" + res[i].price);
+        }
+        console.log("-------------------------------------------");
+        promptUserForProductID();
+    }
+  );
+}
+
+
+function promptUserForProductID(){
+    // queryAllProducts();
+    inquirer
+        .prompt({
+            name: "productID",
+            type: "input",
+            message: "What is the ID of the prouct you would like to buy?",
+            validate: checkForValidInput,
+            filter:Number
+        })
+        .then(function(input){
+            
+        })
+}    
